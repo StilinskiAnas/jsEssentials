9c41c928a23aac85a6e7f78b773d6881c67a6e59
StilinskiAnas <anas.etthabity@edu.uiz.ac.ma>
2026-03-04 17:00:32 -0500

    message

--- SampleFolder/scope_lab.js
+++ SampleFolder/scope_lab.js
@@ -1,33 +1,16 @@
-// Global scope
-var globalVar = "I'm a global variable";
-let globalLet = "I'm also global, but scoped with let";
-const globalConst = "I'm a global constant";
-
-
 {
-// Block scope
-var blockVar = "I'm a block-scoped var";
-let blockLet = "I'm a block-scoped let";
-const blockConst = "I'm a block-scoped const";
+    let a = 5;
+    var b = 4;
+    const c = 3;
+    // a = 10;
+    // b = 14;
+    // c = 13;
 }
 
-// Global scope
-console.log(globalVar); // Output: "I'm a global variable"
-console.log(globalLet); // Output: "I'm also global, but scoped with let"
-console.log(globalConst); // Output: "I'm a global constant"
+    a = 10;
+    b = 14;
+    c = 13;
 
-//Block Scope
-console.log(blockVar);
-console.log(blockLet);
-
-function show(){
-    var functionVar = "I'm a block-scoped var";
-    let functionLet = "I'm a block-scoped let";
-    const functionConst = "I'm a block-scoped const";
-    }
-    show();
-    
-    //console.log(functionVar); // Throws ReferenceError
-    //console.log(functionLet); // Throws ReferenceError
-    console.log(functionConst); // Throws ReferenceError
-    
+console.log(a);
+console.log(b);
+console.log(c);
